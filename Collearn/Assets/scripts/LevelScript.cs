72e30b2710a9a19b23bc0cb730e49b5dde3c8073
MaximDecherf <52461982+MaximDecherf@users.noreply.github.com>
2021-05-10 17:17:24 +0200

    levels

---
+++ Collearn/Assets/scripts/LevelScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelScript : MonoBehaviour
+{
+   
+
+    public void PlayGame1 ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGame2 ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayGame3 ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void PlayGame4 ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void PlayGame5 ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+}
